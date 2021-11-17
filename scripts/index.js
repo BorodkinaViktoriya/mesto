3260cccb8f3cc7ba1caf95844d23c96c2b8e29bd
Viktoriya Borodkina <Motiviruisya@yandex.ru>
2021-11-17 14:37:28 +0300

    fix: errors

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,17 @@ const popup = document.querySelector(".popup");
 const popupClose = popup.querySelector(".popup__close-button");
 let nameProfile = document.querySelector(".profile__name");
 let jobProfile = document.querySelector(".profile__job");
-let formElement = document.querySelector(".popup__container");
+let formElement = document.querySelector(".popup__form");
 const nameInput = popup.querySelector(".popup__input_value_name");
 const jobInput = popup.querySelector(".popup__input_value_job");
 
-function open() {
+function openProfileForm() {
   popup.classList.add("popup_opened");
-  nameInput.setAttribute("value", nameProfile.textContent);
-  jobInput.setAttribute("value", jobProfile.textContent);
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 
-function close() {
+function closeProfileForm() {
   popup.classList.remove("popup_opened");
 }
 
@@ -21,9 +21,9 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popup.classList.remove("popup_opened");
+  closeProfileForm();
 }
 
-editButton.addEventListener("click", open);
-popupClose.addEventListener("click", close);
+editButton.addEventListener("click", openProfileForm);
+popupClose.addEventListener("click", closeProfileForm);
 formElement.addEventListener("submit", formSubmitHandler);
