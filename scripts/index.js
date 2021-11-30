e3592de4f5c2e9f7754d8a551bff8af18930e3bf
Viktoriya Borodkina <Motiviruisya@yandex.ru>
2021-11-30 22:18:04 +0300

    fix:popup

--- scripts/index.js
+++ scripts/index.js
@@ -30,9 +30,9 @@ const templateEl = document.querySelector('template')
 const editButton = document.querySelector('.profile__edit-button');
 const popupProfile = document.querySelector('.popup_contain_profile');
 const popupProfileClose = popupProfile.querySelector('.popup__close-button');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-let profileForm = popupProfile.querySelector('[name="profileForm"]');
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
+const profileForm = popupProfile.querySelector('[name="profileForm"]');
 const nameInput = popupProfile.querySelector('.popup__input_value_name');
 const jobInput = popupProfile.querySelector('.popup__input_value_job');
 const addButton = document.querySelector('.lead__add-button');
@@ -41,64 +41,20 @@ const placeForm = popupPlaces.querySelector('[name="placeForm"]');
 const placesCloseButton = popupPlaces.querySelector('.popup__close-button');
 const placeNameInput = popupPlaces.querySelector('.popup__input_value_place-name');
 const placeLinkInput = popupPlaces.querySelector('.popup__input_value_place-link');
+const popupZoomImages = document.querySelector('.popup_contain_image');
+const imageInPopup = popupZoomImages.querySelector('.popup__image');
+const titleInPopup = popupZoomImages.querySelector('.popup__caption');
+const zoomImagesCloseButton = popupZoomImages.querySelector('.popup__close-button');
 
-function render() {
-  const places = initialPlaces.map((item) => {
-    return getItem(item);
-  });
-  placesContainer.append(...places);
-}
+function handleZoom(event) {
+  openPopup(popupZoomImages)
 
-function getItem(item) {
-  const newItem = templateEl.content.cloneNode(true);
-  const placeTitle = newItem.querySelector('.place__title');
-  const placeImage = newItem.querySelector('.place__image');
-  placeTitle.textContent = item.name;
-  placeImage.src = item.link;
-  placeImage.alt = item.name;
-  const removeButton = newItem.querySelector('.place__remove-button');
-  removeButton.addEventListener( 'click', handleDelete);
-  const likeButton = newItem.querySelector('.place__like-button');
-  likeButton.addEventListener( 'click', handleLike);
-    return newItem;
-}
+  const targetImage = event.target;
+  const placeWithImage = targetImage.closest('.place');
+  const captionInPlace = placeWithImage.querySelector('.place__title');
 
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-function handleProfileForm() {
-  openPopup(popupProfile);
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = jobProfile.textContent;
-}
-
-
-function ProfileSubmitHandler(evt) {
-  evt.preventDefault();
-  nameProfile.textContent = nameInput.value;
-  jobProfile.textContent = jobInput.value;
-  closePopup(popupProfile);
-}
-
-
-function placeSubmitHandler(evt) {
-  evt.preventDefault();
-  const placeName = placeNameInput.value;
-  const placeLink = placeLinkInput.value;
-  if (placeNameInput.value ==="" && placeLinkInput.value ==="") {
-    closePopup(popupPlaces);
-  } else {
-    const placeItem = getItem({name: placeName, link: placeLink});
-    placesContainer.prepend(placeItem);
-    closePopup(popupPlaces);
-    placeNameInput.value = '';
-    placeLinkInput.value = '';
-  }
+  imageInPopup.src = targetImage.src;
+  titleInPopup.textContent = captionInPlace.textContent;
 }
 
 function handleDelete(event) {
@@ -112,13 +68,78 @@ function handleLike(event) {
   targetLike.classList.toggle('place__like-button_active');
 }
 
+// функция создания карточки
+function getItem(item) {
+  const newItem = templateEl.content.cloneNode(true);
+  const placeTitle = newItem.querySelector('.place__title');
+  const placeImage = newItem.querySelector('.place__image');
+  const removeButton = newItem.querySelector('.place__remove-button');
+  const likeButton = newItem.querySelector('.place__like-button');
 
+  placeTitle.textContent = item.name;
+  placeImage.src = item.link;
+  placeImage.alt = item.name;
 
+  removeButton.addEventListener('click', handleDelete);
+  placeImage.addEventListener('click', handleZoom);
+  likeButton.addEventListener('click', handleLike);
+
+  return newItem;
+}
+
+// функция добавления 6 катрочек 'Из коробки'
+function render() {
+  const places = initialPlaces.map((item) => {
+    return getItem(item);
+  });
+  placesContainer.append(...places);
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// функция открытия попапа с формой профиля
+function handleProfileForm() {
+  openPopup(popupProfile);
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+
+// функция сщхранения профиля
+function handleProfileSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup(popupProfile);
+}
+
+// функция сохранения фотографии места
+function handleplaceSubmit(evt) {
+  evt.preventDefault();
+  const placeName = placeNameInput.value;
+  const placeLink = placeLinkInput.value;
+  if (placeNameInput.value === "" && placeLinkInput.value === "") {
+    closePopup(popupPlaces);
+  } else {
+    const placeItem = getItem({name: placeName, link: placeLink});
+    placesContainer.prepend(placeItem);
+    closePopup(popupPlaces);
+    placeNameInput.value = '';
+    placeLinkInput.value = '';
+  }
+}
 
 editButton.addEventListener('click', handleProfileForm);
-addButton.addEventListener('click',()=>openPopup(popupPlaces));
-popupProfileClose.addEventListener('click',()=>closePopup(popupProfile));
-profileForm.addEventListener('submit', ProfileSubmitHandler);
-placeForm.addEventListener('submit', placeSubmitHandler);
-placesCloseButton.addEventListener('click',()=>closePopup(popupPlaces));
+addButton.addEventListener('click', () => openPopup(popupPlaces));
+popupProfileClose.addEventListener('click', () => closePopup(popupProfile));
+profileForm.addEventListener('submit', handleProfileSubmit);
+placeForm.addEventListener('submit', handleplaceSubmit);
+placesCloseButton.addEventListener('click', () => closePopup(popupPlaces));
+zoomImagesCloseButton.addEventListener('click', () => closePopup(popupZoomImages));
+
 render();
